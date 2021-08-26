5f116e0966f2390427ca12a96a367866d11e627a
agaworska <ola.gaworska@gmail.com>
2021-08-26 18:13:57 +0200

    insertion sort - add two other implemetations

--- insertion/insertion.py
+++ insertion/insertion.py
@@ -1,15 +1,16 @@
 
+def swap(list_to_sort, idx):
+    prev_idx = idx - 1
+    current_elem = list_to_sort[idx]
+    list_to_sort[idx] = list_to_sort[prev_idx]
+    list_to_sort[prev_idx] = current_elem
+
+
 def insertion_sort_recursive(not_sorted):
     list_to_sort = not_sorted.copy()
 
     if len(list_to_sort) == 0:
         return list_to_sort
-
-    def swap(list_to_sort, idx):
-        prev_idx = idx - 1
-        current_elem = list_to_sort[idx]
-        list_to_sort[idx] = list_to_sort[prev_idx]
-        list_to_sort[prev_idx] = current_elem
 
     def recursive_insertion(list_to_sort, idx):
         if idx == 0:
@@ -22,9 +23,9 @@ def insertion_sort_recursive(not_sorted):
 
     current_elem = list_to_sort[0]
 
-    for j, elem in enumerate(list_to_sort[1:], 1):
+    for i, elem in enumerate(list_to_sort[1:], 1):
         if elem < current_elem:
-            recursive_insertion(list_to_sort, j)
+            recursive_insertion(list_to_sort, i)
         else:
             current_elem = elem
 
@@ -32,4 +33,24 @@ def insertion_sort_recursive(not_sorted):
 
 
 def insertion_sort_for(not_sorted):
-    pass
+    list_to_sort = not_sorted.copy()
+
+    for i, main_elem in enumerate(list_to_sort):
+        idx = i
+        for elem in list_to_sort[i::-1]:
+            if main_elem < elem:
+                swap(list_to_sort, idx)
+                idx -= 1
+
+    return list_to_sort
+
+
+def insertion_sort_for_range(not_sorted):
+    list_to_sort = not_sorted.copy()
+
+    for i, main_elem in enumerate(list_to_sort):
+        for j in range(i - 1, -1, -1):
+            if main_elem < list_to_sort[j]:
+                swap(list_to_sort, j + 1)
+
+    return list_to_sort
--- tests/test_insertion.py
+++ tests/test_insertion.py
@@ -2,7 +2,8 @@ import pytest
 
 from insertion.insertion import (
     insertion_sort_recursive,
-    insertion_sort_for
+    insertion_sort_for,
+    insertion_sort_for_range,
 )
 
 from tests.conftest import test_cases
@@ -19,3 +20,8 @@ def test_insertion_recursive(not_sorted, expected):
 @pytest.mark.parametrize('not_sorted, expected', test_cases)
 def test_insertion_for(not_sorted, expected):
     assert insertion_sort_for(not_sorted) == expected
+
+
+@pytest.mark.parametrize('not_sorted, expected', test_cases)
+def test_insertion_for_range(not_sorted, expected):
+    assert insertion_sort_for_range(not_sorted) == expected
